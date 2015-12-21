6fb385850cbe4276c7ae7e8c0d872a4800aec238
Brandon Kvarda <bjkvarda@gmail.com>
2015-12-21 10:53:22 -0800

    Added unique column parsing and removal of special characters to cmcsv

--- capycityplanr/cmcsv.py
+++ capycityplanr/cmcsv.py
@@ -1,4 +1,4 @@
-import logging, os, csv, zipfile, contextlib, shutil
+import logging, os, csv, zipfile, contextlib, shutil, tempfile
 
 
 def scan_folder(folder):
@@ -36,23 +36,65 @@ def extract_csvs(file,output_folder):
     logging.info('List of files to process :' + str(csvs_to_process))
     return csvs_to_process
 
-def clean_csv(csv,output_folder):
+def clean_csv(data,output_folder):
     #Takes dictionary of csv type and location and cleans up so that columns are unique and fit Hive schema
-    customer_name = csv['customer']
-    type = csv['type']
-    file = csv['path']
+    customer_name = data['customer']
+    type = data['type']
+    file = data['path']
+    service_accounts = []
+    user_accounts = []
+    columns = {}
+    unique_columns = []
     logging.info('Cleaning CSV ' + os.path.basename(file))
     logging.info('CSV is type ' + type + ' for customer ' + customer_name)
     #Here we get rid of special characters such as ^M that the csv library can't handle
     file_backup = file + '.bak'
+    with tempfile.NamedTemporaryFile(delete=False) as fh:
+        for line in open(file):
+            line = line.rstrip()
+            line = line.replace('\r', '')
+            fh.write(line + '\n')
     os.rename(file, file_backup)
-    with open(file_backup, 'rU') as infile:
-        with open(file, 'w') as outfile:
-            for line in infile:
-                outfile.write(line.replace('\r', ''))
-    os.remove(file_backup)
-    
+    os.rename(fh.name, file)    
+    os.remove(file_backup) 
     #Here we parse the columns and get rid of not-unique columns and formatting that doesn't work in Hive
+    counter = 0
+    os.rename(file, file_backup)
+    with open (file_backup, 'rU') as input:
+        with open (file, 'wb') as output:
+            writer = csv.writer(output)
+            for row in csv.reader(input):
+                #hdfs type has csv header, this effectively removes it
+                if counter == 0 and type == 'hdfs':
+                    pass 
+                #these are columns that need to be parsed
+                elif (counter == 0 and type != 'hdfs') or (counter == 1 and type == 'hdfs'):
+                    index = 0
+                    for column in row:
+                        #if column not unique
+                        if str(columns.get(column.lower())) != 'None':
+                            #make it unique
+                            logging.info(column + ' is not unique. Appending index')
+                            new_column_name = column + str(index)
+                            columns[new_column_name] = index
+                        #else column is unique
+                        else:
+                            columns[column] = index
+                        
+                        index += 1
+                    #Now we have a dict with unique columns, we need to order them correctly and remove special characters
+                    for column in columns:
+                        clean_column = column.translate(None,'\\*./!?#- ')
+                        number = columns[column]
+                        unique_columns.insert(number, str(clean_column))
+                    logging.info('Inserting the following unique columns into the CSV: ' + str(unique_columns))
+                    writer.writerow(unique_columns)        
+                #this are all data rows that can just be inserted
+                else:
+                    writer.writerow(row)
+                
+                counter += 1
+    os.remove(file_backup)
 
 def get_csv_type(csv):
     if "hdfs" in csv.lower():
@@ -63,5 +105,4 @@ def get_csv_type(csv):
         return "yarn"
     else:
         return "unknown"
-
 
