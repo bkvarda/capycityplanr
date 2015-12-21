d5fe2cebe929e42a8e560c34cb13b22a72d8463b
Brandon Kvarda <bjkvarda@gmail.com>
2015-12-21 08:23:31 -0800

    Added removal of ^M character to cmcsv

--- capycityplanr/cmcsv.py
+++ capycityplanr/cmcsv.py
@@ -38,12 +38,20 @@ def extract_csvs(file,output_folder):
 
 def clean_csv(csv,output_folder):
     #Takes dictionary of csv type and location and cleans up so that columns are unique and fit Hive schema
-    logging.info('Cleaning CSV ' +os.path.basename(csv['path']))
     customer_name = csv['customer']
     type = csv['type']
+    file = csv['path']
+    logging.info('Cleaning CSV ' + os.path.basename(file))
     logging.info('CSV is type ' + type + ' for customer ' + customer_name)
     #Here we get rid of special characters such as ^M that the csv library can't handle
-
+    file_backup = file + '.bak'
+    os.rename(file, file_backup)
+    with open(file_backup, 'rU') as infile:
+        with open(file, 'w') as outfile:
+            for line in infile:
+                outfile.write(line.replace('\r', ''))
+    os.remove(file_backup)
+    
     #Here we parse the columns and get rid of not-unique columns and formatting that doesn't work in Hive
 
 def get_csv_type(csv):
